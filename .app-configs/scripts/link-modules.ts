dbc9387c48b5c61cbab6505df586ed8b225c1e3e
Osama-you <osama.w.younis@gmail.com>
2024-12-05 16:10:27 +0200

    Update App configs

--- .app-configs/scripts/link-modules.ts
+++ .app-configs/scripts/link-modules.ts
@@ -1,15 +1,40 @@
 import fs from 'fs';
-
 import webpackPaths from '../configs/webpack.paths';
 
 const { srcNodeModulesPath, appNodeModulesPath, erbNodeModulesPath } =
   webpackPaths;
 
+// Function to safely create a symlink
+const createSymlink = (target: string, path: string, type: fs.symlink.Type) => {
+  try {
+    if (fs.existsSync(path)) {
+      const stat = fs.lstatSync(path);
+      if (stat.isSymbolicLink()) {
+        const resolvedTarget = fs.readlinkSync(path);
+        if (resolvedTarget === target) {
+          console.log(
+            `Symlink already exists and points to the correct target: ${path}`,
+          );
+          return;
+        } else {
+          console.log(
+            `Symlink exists but points to a different target. Removing: ${path}`,
+          );
+          fs.unlinkSync(path);
+        }
+      } else {
+        console.log(`Path exists but is not a symlink. Removing: ${path}`);
+        fs.rmSync(path, { recursive: true, force: true });
+      }
+    }
+    fs.symlinkSync(target, path, type);
+    console.log(`Symlink created: ${path} -> ${target}`);
+  } catch (err: any) {
+    console.error(`Error creating symlink: ${err.message}`);
+  }
+};
+
 if (fs.existsSync(appNodeModulesPath)) {
-  if (!fs.existsSync(srcNodeModulesPath)) {
-    fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
-  }
-  if (!fs.existsSync(erbNodeModulesPath)) {
-    fs.symlinkSync(appNodeModulesPath, erbNodeModulesPath, 'junction');
-  }
+  createSymlink(appNodeModulesPath, srcNodeModulesPath, 'junction');
+  createSymlink(appNodeModulesPath, erbNodeModulesPath, 'junction');
 }
